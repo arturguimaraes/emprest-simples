4e72149083b5f50069d3722cb513e1ce64ff0ff7
Artur Guimarães <arturguimaraes92@hotmail.com>
2026-07-16 16:42:52 -0300

    Versioning control

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { execSync } from 'child_process';
+
+const commitSha = execSync('git rev-parse --short HEAD').toString().trim();
+const buildDate = new Date().toISOString().split('T')[0];
 
 export default defineConfig({
   plugins: [react()],
   base: '/emprest-simples/',
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __COMMIT_SHA__: JSON.stringify(commitSha),
+    __BUILD_DATE__: JSON.stringify(buildDate),
+  },
 });
